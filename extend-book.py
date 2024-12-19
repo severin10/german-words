fd6fe5ff09c434dce8bdcf6dc8969197ca620c8a
severin10 <severin.dietschi@gmx.ch>
2024-12-19 15:54:26 +0100

    WIP

--- extend-book.py
+++ extend-book.py
@@ -3,16 +3,16 @@
 import json, os, re
 from pprint import pprint 
 
-os.system("cls")    # Windows
-# os.system("clear")  # Linux
+# os.system("cls")    # Windows
+os.system("clear")  # Linux
 
 
 with open('German-words-1600000-words.json', 'r') as file:
     words = json.load(file)
 
-beginsWithHalb = []
-endsWithBuch = []
+def addWord(wordStr):
+    words.append(wordStr)
+    words.sort()
 
 
-
-pprint(words)
+addWord('Dammhaus')
